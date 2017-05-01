56fc7c674211a9ae0d01b639d080017773d9278d
aBondurant <abondurant1@radford.edu>
2017-05-01 18:01:37 -0400

    added a timer

--- php/RuInput.php
+++ php/RuInput.php
@@ -12,24 +12,38 @@
     </head>
     <script>
         html = document.documentElement;
-        var o = new TargetQueue(html.offsetWidth,  html.offsetHeight , 6);
+        var o = new TargetQueue(html.offsetWidth,  html.offsetHeight , 30);
         o.generateTargets();
         alert(JSON.stringify(o, null, 2));
+		
+		var date = new Date();
+		var initialTime = date.getTime();
+		
+		
 
         function generateTarget() {
             var target = document.createElement("BUTTON");
+			var date = new Date();
+			var newTargetTime = date.getTime();
             var values = o.dequeueTarget();
             target.className = "target";
             target.id = "target";
             target.onclick = function () {
                 removeTarget(this.id);
+				var clickedDate = new Date();
+				var clickedtime = clickedDate.getTime();
+				timeToClick = clickedtime - newTargetTime;
+				alert(timeToClick);
             };
+			
+			
             target.style.height = Math.floor(values.size) + "px";
             target.style.width = Math.floor(values.size) + "px";
             target.style.top = Math.floor(values.yPos) + "px" ;
             target.style.left = Math.floor(values.xPos ) + "px";
 
             document.getElementById("targetContainer").appendChild(target);
+			
         }
 
         function removeTarget(id) {
